a15caafa02dd58e9f98c737e915e2f0278d1ed89
halileryildirim <83421076+halileryildirim@users.noreply.github.com>
2023-12-20 03:53:07 +0300

    Added queue, rewrote all the functions in a single knightMoves function

--- index.js
+++ index.js
@@ -1,17 +1,40 @@
-function chessBoard() {
-  const gameBoard = [];
-  for (let i = 0; i < 8; i += 1) {
-    for (let j = 0; j < 8; j += 1) {
-      gameBoard.push([i, j]);
-    }
+class Queue {
+  constructor() {
+    this.nodes = {};
+    this.head = 0;
+    this.tail = 0;
   }
 
-  return gameBoard;
+  enqueue(node) {
+    this.nodes[this.tail] = node;
+    this.tail += 1;
+  }
+
+  dequeue() {
+    const item = this.nodes[this.head];
+    delete this.nodes[this.head];
+    this.head += 1;
+    return item;
+  }
+
+  peek() {
+    return this.nodes[this.head];
+  }
+
+  get length() {
+    return this.tail - this.head;
+  }
+
+  get isEmpty() {
+    return this.length === 0;
+  }
 }
 
-function knightMoveGenerate(row, col) {
-  const boardSize = 8;
-  const moves = [
+function knightMoves(start, end) {
+  const queue = new Queue();
+  const visited = new Set();
+
+  const moveSet = [
     { row: -2, col: -1 },
     { row: -2, col: 1 },
     { row: -1, col: -2 },
@@ -21,24 +44,33 @@ function knightMoveGenerate(row, col) {
     { row: 2, col: -1 },
     { row: 2, col: 1 },
   ];
-  const validMoves = [];
-  for (move in moves) {
-    const newRow = row + moves[move].row;
-    const newCol = col + moves[move].col;
-    if (
-      newRow >= 0 &&
-      newRow < boardSize &&
-      newCol >= 0 &&
-      newCol < boardSize
-    ) {
-      validMoves.push({ row: newRow, col: newCol });
-    }
+
+  while (start !== end) {
+    queue.enqueue(start);
+    if (start !== end)
+      for (const move in moveSet) {
+        const row = moveSet[move].row + start.row;
+        const col = moveSet[move].col + start.col;
+        const node = { row, col };
+        if (node !== start && node === end) {
+          queue.enqueue(node);
+          const oldNode = queue.dequeue();
+          visited.add(oldNode);
+          return;
+        }
+        if (node.col < 7 && node.col >= 0 && node.row >= 0 && node.row < 7) {
+          queue.enqueue(node);
+          const oldNode = queue.dequeue();
+          visited.add(oldNode);
+        }
+      }
   }
-  return validMoves;
+  const visitArray = Array.from(visited);
+  for (const i in visitArray) {
+    console.log(visitArray[i]);
+  }
 }
 
-const board = chessBoard();
-const possibleMoves = knightMoves(5, 3);
-for (move in possibleMoves) {
-  console.log(possibleMoves[move]);
-}
+const start = { row: 0, col: 0 };
+const end = { row: 1, col: 2 };
+knightMoves(start, end);
